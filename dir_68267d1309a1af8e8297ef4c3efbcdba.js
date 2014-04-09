5eeca39274643b35c575f589c60aa065d1efd73e
Allan Clark <allanc@chickenandporn.com>
2014-04-09 12:35:53 -0700

    Add documentation for C code (largely based on xmlsoft examples)

---
+++ dir_68267d1309a1af8e8297ef4c3efbcdba.js
@@ -0,0 +1,7 @@
+var dir_68267d1309a1af8e8297ef4c3efbcdba =
+[
+    [ "rpm2xml.c", "rpm2xml_8c_source.html", null ],
+    [ "xml2rss.xsl", "xml2rss_8xsl_source.html", null ],
+    [ "xmlsoft-xpath1.c", "xmlsoft-xpath1_8c_source.html", null ],
+    [ "xmlsoft-xpath2.c", "xmlsoft-xpath2_8c_source.html", null ]
+];
--- files.js
+++ files.js
@@ -1,4 +1,5 @@
 var files =
 [
-    [ "java", "dir_93c6ba7fb77bae0709fae89c9c13b44a.html", "dir_93c6ba7fb77bae0709fae89c9c13b44a" ]
+    [ "java", "dir_93c6ba7fb77bae0709fae89c9c13b44a.html", "dir_93c6ba7fb77bae0709fae89c9c13b44a" ],
+    [ "src", "dir_68267d1309a1af8e8297ef4c3efbcdba.html", "dir_68267d1309a1af8e8297ef4c3efbcdba" ]
 ];
